0bdd4d2ebd82e7c322eed51e9e6179691b8c57e2
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2023-12-17 18:44:52 +0200

    Update Pair.java
    Update Pair

--- Employees/src/main/java/com/example/Employees/util/Pair.java
+++ Employees/src/main/java/com/example/Employees/util/Pair.java
@@ -1,7 +1,10 @@
 package com.example.Employees.util;
-
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Objects;
+
+@Data
 @EqualsAndHashCode
 public class Pair {
     private final Long employeeId1;
@@ -11,8 +14,24 @@ public class Pair {
         this.employeeId1 = employeeId1;
         this.employeeId2 = employeeId2;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return (employeeId1.equals(pair.employeeId1) && employeeId2.equals(pair.employeeId2)) ||
+                (employeeId1.equals(pair.employeeId2) && employeeId2.equals(pair.employeeId1));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employeeId1, employeeId2) + Objects.hash(employeeId2, employeeId1);
+    }
+
     @Override
     public String toString() {
         return "Pair{" + "employeeId1=" + employeeId1 + ", employeeId2=" + employeeId2 + '}';
     }
 }
+
