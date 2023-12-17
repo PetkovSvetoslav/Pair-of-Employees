3542366c071eac2be30e368e56b8f5c4b3443680
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2023-12-17 18:49:16 +0200

    Update Employee.java

--- Employees/src/main/java/com/example/Employees/model/entity/Employee.java
+++ Employees/src/main/java/com/example/Employees/model/entity/Employee.java
@@ -3,7 +3,6 @@ package com.example.Employees.model.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
