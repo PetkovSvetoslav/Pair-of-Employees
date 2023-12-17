1cb1e8a584a3bbba96c7d24360538d6acc6fdea2
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2023-12-17 15:41:38 +0200

    Update EmployeeProjectController.java
    Update EmployeeProjectController

--- Employees/src/main/java/com/example/Employees/controller/EmployeeProjectController.java
+++ Employees/src/main/java/com/example/Employees/controller/EmployeeProjectController.java
@@ -1,18 +1,46 @@
 package com.example.Employees.controller;
 
+import org.springframework.ui.Model;
+import com.example.Employees.exception.DomainException;
+import com.example.Employees.model.entity.EmployeeProject;
+import com.example.Employees.repository.EmployeeProjectRepository;
+import com.example.Employees.service.CSVParserService;
 import com.example.Employees.service.EmployeeProjectService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employeeProjects")
 public class EmployeeProjectController {
-
     private final EmployeeProjectService employeeProjectService;
+    private final CSVParserService csvParserService;
+    private final EmployeeProjectRepository employeeProjectRepository;
 
-    public EmployeeProjectController(EmployeeProjectService employeeProjectService) {
+    public EmployeeProjectController(EmployeeProjectService employeeProjectService,
+                                     CSVParserService csvParserService,
+                                     EmployeeProjectRepository employeeProjectRepository) {
         this.employeeProjectService = employeeProjectService;
+        this.csvParserService = csvParserService;
+        this.employeeProjectRepository = employeeProjectRepository;
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, Model model) {
+        try {
+            List<EmployeeProject> employeeProjects = csvParserService.parseCSVFile(file);
+            employeeProjectRepository.saveAll(employeeProjects);
+            List<String> pairs = employeeProjectService.findLongestWorkingPairs();
+            model.addAttribute("pairs", pairs);
+        } catch (DomainException e) {
+            model.addAttribute("error", "Error processing file: " + e.getMessage());
+        }
+        return "index";
+    }
 }
